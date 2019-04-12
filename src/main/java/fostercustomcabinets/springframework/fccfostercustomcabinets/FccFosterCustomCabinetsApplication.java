801636497d17f5b53d909b5deeff3226e0687747
Jake Hoard <jacob37857@gmail.com>
2019-04-12 04:17:27 -0400

    Initial Project Add

---
+++ src/main/java/fostercustomcabinets/springframework/fccfostercustomcabinets/FccFosterCustomCabinetsApplication.java
@@ -0,0 +1,14 @@
+package fostercustomcabinets.springframework.fccfostercustomcabinets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class FccFosterCustomCabinetsApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FccFosterCustomCabinetsApplication.class, args);
+	}
+
+}
