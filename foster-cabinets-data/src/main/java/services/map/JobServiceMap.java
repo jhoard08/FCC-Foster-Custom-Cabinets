6db0157eaacc2d4c7e866cc969302c0915f9ba22
Jake Hoard <jacob37857@gmail.com>
2019-04-16 11:25:15 -0400

    Added Index Controller. close #8

--- foster-cabinets-data/src/main/java/services/map/JobServiceMap.java
+++ foster-cabinets-data/src/main/java/services/map/JobServiceMap.java
@@ -8,27 +8,32 @@ import java.util.Set;
 public class JobServiceMap extends AbstractMapService<Job, Long> implements CrudService<Job, Long>
 {
 
-	@Override public Set<Job> findAll()
+	@Override
+	public Set<Job> findAll()
 	{
 		return super.findAll();
 	}
 
-	@Override public void delete(Job object)
+	@Override
+	public void delete(Job object)
 	{
 		super.delete(object);
 	}
 
-	@Override public Job save(Job object)
+	@Override
+	public Job save(Job object)
 	{
 		return super.save(object.getId(), object);
 	}
 
-	@Override public void deleteById(Long id)
+	@Override
+	public void deleteById(Long id)
 	{
 		super.deleteById(id);
 	}
 
-	@Override public Job findById(Long id)
+	@Override
+	public Job findById(Long id)
 	{
 		return super.findById(id);
 	}
