d313cf803071c496bcff9cb68c34286c9bd8f038
Jake <jacob37857@gmail.com>
2019-04-20 23:42:24 -0400

    Added laborers List and updated controller. closes #17

--- foster-cabinets-web/src/main/java/fostercustomcabinets/fcc/controllers/LaborersController.java
+++ foster-cabinets-web/src/main/java/fostercustomcabinets/fcc/controllers/LaborersController.java
@@ -1,13 +1,25 @@
 package fostercustomcabinets.fcc.controllers;
 
+import fostercustomcabinets.fcc.services.LaborerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/laborers")
 @Controller
 public class LaborersController
 {
-	@RequestMapping({"/laborers", "/laborers/index", "/laborers/index.html"})
-	public String listLaborers(){
+	private final LaborerService laborerService;
+
+	public LaborersController(LaborerService laborerService) {
+		this.laborerService = laborerService;
+	}
+
+	@RequestMapping({"","/","/laborers", "/laborers/index", "/laborers/index.html"})
+	public String listLaborers(Model model){
+
+		model.addAttribute("laborers", laborerService.findAll());
+
 		return "laborers/index";
 	}
 }
