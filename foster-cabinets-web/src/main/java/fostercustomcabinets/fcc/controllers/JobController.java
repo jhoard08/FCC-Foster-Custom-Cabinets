52f76f5cea4dad80a7a060a75cee5ab7b86ae98a
Jake <jacob37857@gmail.com>
2019-04-20 23:00:59 -0400

    Added Jobs List and updated controller. closes #16

--- foster-cabinets-web/src/main/java/fostercustomcabinets/fcc/controllers/JobController.java
+++ foster-cabinets-web/src/main/java/fostercustomcabinets/fcc/controllers/JobController.java
@@ -1,13 +1,25 @@
 package fostercustomcabinets.fcc.controllers;
 
+import fostercustomcabinets.fcc.services.JobService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/jobs")
 @Controller
 public class JobController
 {
-	@RequestMapping({"/jobs","jobs/index","/jobs/index.html"})
-	public String jobList(){
+	private final JobService jobService;
+
+	public JobController(JobService jobService) {
+		this.jobService = jobService;
+	}
+
+	@RequestMapping({"","/","/jobs","/jobs/index","/jobs/index.html"})
+	public String jobList(Model model){
+
+		model.addAttribute("jobs", jobService.findAll());
+
 		return "jobs/index";
 	}
 }
