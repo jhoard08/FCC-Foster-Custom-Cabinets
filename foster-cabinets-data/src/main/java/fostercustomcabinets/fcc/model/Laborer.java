247a04d135b83664077c24e721a7693f713bb293
Jake <jacob37857@gmail.com>
2019-04-21 00:06:34 -0400

    Added pay to laborers

--- foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/model/Laborer.java
+++ foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/model/Laborer.java
@@ -2,4 +2,13 @@ package fostercustomcabinets.fcc.model;
 
 public class Laborer extends Person
 {
+    private long pay;
+
+    public long getPay() {
+        return pay;
+    }
+
+    public void setPay(long pay) {
+        this.pay = pay;
+    }
 }
