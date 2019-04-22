1022fee9a424896340565140334f2f3f952fc31b
Jake <jacob37857@gmail.com>
2019-04-21 20:50:44 -0400

    added specialties, added to laborers. closes #27

--- foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/model/Laborer.java
+++ foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/model/Laborer.java
@@ -1,7 +1,19 @@
 package fostercustomcabinets.fcc.model;
 
+import java.util.Set;
+
 public class Laborer extends Person
 {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
     private long pay;
 
     public long getPay() {
