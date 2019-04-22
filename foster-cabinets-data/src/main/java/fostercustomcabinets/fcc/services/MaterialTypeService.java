1a5fcade977b64610d960feb2c2335c8f9c1615b
Jake <jacob37857@gmail.com>
2019-04-21 21:04:02 -0400

    added MaterialTypeService. closes #29

---
+++ foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/services/MaterialTypeService.java
@@ -0,0 +1,7 @@
+package fostercustomcabinets.fcc.services;
+
+import fostercustomcabinets.fcc.model.MaterialType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MaterialTypeService extends CrudService<MaterialType, Long> {
+}
---
+++ foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/services/map/MaterialTypeMapService.java
@@ -0,0 +1,35 @@
+package fostercustomcabinets.fcc.services.map;
+
+import fostercustomcabinets.fcc.model.MaterialType;
+import fostercustomcabinets.fcc.services.MaterialTypeService;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+@Service
+public class MaterialTypeMapService extends AbstractMapService<MaterialType, Long> implements MaterialTypeService {
+    @Override
+    public Set<MaterialType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public MaterialType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public MaterialType save(MaterialType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(MaterialType object) {
+        super.delete(object);
+    }
+
+}
