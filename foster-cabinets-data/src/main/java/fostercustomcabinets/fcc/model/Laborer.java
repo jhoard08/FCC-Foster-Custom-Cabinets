c7ee176cbd16faace549e6e9fe701b86c7ac5c0c
Jake <jacob37857@gmail.com>
2019-04-24 17:07:54 -0400

    updated laborers with JPA entity mapping. closes #37

--- foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/model/Laborer.java
+++ foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/model/Laborer.java
@@ -1,11 +1,19 @@
 package fostercustomcabinets.fcc.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "laborers")
 public class Laborer extends Person
 {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "labor_specialities", joinColumns = @JoinColumn(name = "labor_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialties = new HashSet<>();
+
+    @Column(name = "pay")
     private long pay;
 
     public Set<Specialty> getSpecialties() {
