855899703c226edba48a8ac8670778e027ca8a52
Jake <jacob37857@gmail.com>
2019-04-26 15:10:41 -0400

    added spring data jpa service for specialty. closes #44

---
+++ foster-cabinets-data/src/main/java/fostercustomcabinets/fcc/services/springdatajpa/SpecailtySDJpaService.java
@@ -0,0 +1,52 @@
+package fostercustomcabinets.fcc.services.springdatajpa;
+
+import fostercustomcabinets.fcc.model.Specialty;
+import fostercustomcabinets.fcc.repositories.SpecialtyRepository;
+import fostercustomcabinets.fcc.services.SpecialitiesService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecailtySDJpaService implements SpecialitiesService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecailtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+
+        Set<Specialty> specialties = new HashSet<>();
+
+        specialtyRepository.findAll().forEach(specialties::add);
+
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
